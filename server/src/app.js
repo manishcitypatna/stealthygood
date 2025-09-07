e4a2f91c864624fd52df93ddad176a6f33c403e9
ManishCityPatna <manipritraj@gmail.com>
2025-09-07 17:17:52 +0530

    fix: update CORS to allow vercel frontend

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://stealthygood.vercel.app'],
   credentials: true,
 }));
 
